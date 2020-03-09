1f9d0bc70b6a01b75d77f6490d62da10717c0deb
BenzinasDujos <abraitis.adomas@gmail.com>
2020-03-09 17:13:51 +0200

    Works on postman

---
+++ server/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/','MovieController@getAll');
+Route::post('/','MovieController@post');
+Route::delete('/{id}','MovieController@delete');
+Route::put('/{id}','MovieController@update');
+Route::get('/{id}','MovieController@getOne');
